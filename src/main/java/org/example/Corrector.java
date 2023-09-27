7e0ff82b5ef214a1da01144f4839435b5579c24f
Łukasz Czajka <lukasz.czajka48@gmail.com>
2023-09-27 22:38:11 +0200

    fixed multiple space
    reworking the deleteSpace method to remove repeated spaces before and after punctuation

--- src/main/java/org/example/Corrector.java
+++ src/main/java/org/example/Corrector.java
@@ -10,7 +10,9 @@ public class Corrector {
 
     public void correcting(String content){
         toList(content);
-        deleteSpaceBefore();
+        deleteLastDot();// jeśli ostatni znak listy / wprowadzonego tekstu jest , lub . - usuwamy go
+                        // jeśli tego nie usuniemy metody poniżej pokażą błędy ponieważ próbują uzyskać dostęp do indeksu i+2, który w przypadku gdy '.' jest na końcu wychodzi poza granice listy
+        deleteSpaces();
         addSpaceAfter();
         changeToCapitalLetter();
     }
@@ -22,14 +24,30 @@ public class Corrector {
         }
     }
 
-    //usunięcie spacji przed znakiem , || .
-    public void deleteSpaceBefore(){//usunięcie spacji przed znakiem , || .
+    //usunięcie . jako ostniego elementu Stringa
+    public void deleteLastDot(){
+        if (listOfLetterContent.get(listOfLetterContent.size()-1) == '.' || listOfLetterContent.get(listOfLetterContent.size()-1) == ','){
+            listOfLetterContent.remove(listOfLetterContent.size()-1);
+            deleteLastDot();
+        }
+    }
+
+    //usunięcie spacji przed i po znakach , || .
+    public void deleteSpaces(){//usunięcie spacji przed znakiem , || .
         for (int i = 0; i < listOfLetterContent.size(); i++){
             char currentChar = listOfLetterContent.get(i);
+           // char lastElement = listOfLetterContent.get(listOfLetterContent.size()-1);
+
             if ((currentChar == ',' || currentChar == '.') && listOfLetterContent.get(i-1) == ' ' ){
-                //sprawdza, czy znak jest , || . oraz czy poprzedzający znak(i-1) == spacja
+                //sprawdza czy znak przed , lub . jest spacją
                 //jeśli tak, usuń
                 listOfLetterContent.remove(i-1);
+                i-=2; // ustawia i pętli po całej liscie -2 aby sprawdzić czy nie ma więcej niż 1 spacji
+            } else if ((currentChar == ',' || currentChar == '.') && listOfLetterContent.get(i+2) == ' '){
+                //sprawdza czy drugi znak po , lub . jest spacją
+                //jeśli tak, usuń
+                listOfLetterContent.remove(i + 2);
+                i-=2; // ustawia i pętli po całej liscie -2 aby sprawdzić czy nie ma więcej niż 1 spacji
             }
         }
     }
@@ -58,11 +76,11 @@ public class Corrector {
         }
     }
 
-    public void charTooString(){
+    public String charTooString(){
         for (Character c : listOfLetterContent){
             correctedContent += c;
         }
-        System.out.println(correctedContent);
+        return correctedContent;
     }
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,13 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        String content = "To jest przykład zdania . czasem występują błędy , ale nie zawsze .Ważne jest ,aby zadbać o poprawność tekstu .Inaczej może być trudno zrozumieć treść .Tutaj jest jeszcze jedno zdanie bez wielkiej litery na początku .Podobnie jak to ,które jest obok .Teraz nastąpi poprawne zdanie .Napisane bez błędów interpunkcyjnych .Oczywiście nie zawsze to się zdarza .Ale to nie problem .Mamy teraz 50% błędów interpunkcyjnych i braku wielkich liter.tutaj jest kolejny przykład .Przecinki i kropki są nieprawidłowo umieszczone .Jest to prawdziwe wyzwanie dla czytelnika .Kiedyś będziemy pisali lepiej .\n";
+        String content = "To jest przykład zdania    ,              123 . test";
 
         Corrector corrector = new Corrector();
         corrector.correcting(content);
-        corrector.charTooString();
+        System.out.println(corrector.charTooString());
 
+        //,trim() - usunięcie spacji z końca i początku
 
     }
 }
