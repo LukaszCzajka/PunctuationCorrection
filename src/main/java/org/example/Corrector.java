05be825200755b9abb17f57cd3581258adf95c6c
Łukasz Czajka <lukasz.czajka48@gmail.com>
2023-09-28 22:42:52 +0200

    Optymalization and added removal multiple spaces without punctuation

--- src/main/java/org/example/Corrector.java
+++ src/main/java/org/example/Corrector.java
@@ -10,11 +10,12 @@ public class Corrector {
 
     public void correcting(String content){
         toList(content);
-        deleteLastDot();// jeśli ostatni znak listy / wprowadzonego tekstu jest , lub . - usuwamy go
-                        // jeśli tego nie usuniemy metody poniżej pokażą błędy ponieważ próbują uzyskać dostęp do indeksu i+2, który w przypadku gdy '.' jest na końcu wychodzi poza granice listy
-        deleteSpaces();
-        addSpaceAfter();
-        changeToCapitalLetter();
+        for (int i = 0; i < listOfLetterContent.size() -2; i++){ // pomniejszamy iteracje o rozmiar listy -2
+            //jeśli tego nie zrobimy metody poniżej pokażą błędy ponieważ próbują uzyskać dostęp do indeksu i+2, który w przypadku gdy '.' jest na końcu wychodzi poza granice listy
+            i = deleteSpaces(i); // ustawia i-=2 żeby sprawdzić czy nie ma więcej spacji
+            addSpaceAfter(i);
+            changeToCapitalLetter(i);
+        }
     }
 
     //zmiana stinga na liste char
@@ -24,56 +25,48 @@ public class Corrector {
         }
     }
 
-    //usunięcie . jako ostniego elementu Stringa
-    public void deleteLastDot(){
-        if (listOfLetterContent.get(listOfLetterContent.size()-1) == '.' || listOfLetterContent.get(listOfLetterContent.size()-1) == ','){
-            listOfLetterContent.remove(listOfLetterContent.size()-1);
-            deleteLastDot();
-        }
-    }
-
     //usunięcie spacji przed i po znakach , || .
-    public void deleteSpaces(){//usunięcie spacji przed znakiem , || .
-        for (int i = 0; i < listOfLetterContent.size(); i++){
+    public int deleteSpaces(int i){
             char currentChar = listOfLetterContent.get(i);
-           // char lastElement = listOfLetterContent.get(listOfLetterContent.size()-1);
-
             if ((currentChar == ',' || currentChar == '.') && listOfLetterContent.get(i-1) == ' ' ){
-                //sprawdza czy znak przed , lub . jest spacją
-                //jeśli tak, usuń
+                //sprawdza czy znak przed , lub . jest spacji - jeśli tak, usuń
                 listOfLetterContent.remove(i-1);
-                i-=2; // ustawia i pętli po całej liscie -2 aby sprawdzić czy nie ma więcej niż 1 spacji
+                i-=2; // ustawia i do zwrócenia w pętli po całej liscie w metodzie correcting. -2 aby sprawdzić czy nie ma więcej niż 1 spacji
             } else if ((currentChar == ',' || currentChar == '.') && listOfLetterContent.get(i+2) == ' '){
                 //sprawdza czy drugi znak po , lub . jest spacją
                 //jeśli tak, usuń
                 listOfLetterContent.remove(i + 2);
+                i-=2; // ustawia i do zwrócenia w pętli po całej liscie w metodzie correcting +2 aby sprawdzić czy nie ma więcej niż 1 spacji
+            } else if (currentChar == ' ' && listOfLetterContent.get(i-1) == ' ' ){
+                //usuwa zdublowane spacje między którymi nie ma interpunkcji
+                listOfLetterContent.remove(i-1);
                 i-=2; // ustawia i pętli po całej liscie -2 aby sprawdzić czy nie ma więcej niż 1 spacji
             }
-        }
+        return i;
     }
 
     //dodanie spacji po znaku , || .
-    public void addSpaceAfter(){
-        for (int i = 0; i < listOfLetterContent.size(); i++){
+    public void addSpaceAfter(int i){
+        //for (int i = 0; i < listOfLetterContent.size() - 2; i++){
             char currentChar = listOfLetterContent.get(i);
             if ((currentChar == ',' || currentChar == '.') && listOfLetterContent.get(i+1) != ' '){
                 //sprawdza, czy znak jest , || . oraz czy następny znak(i+1) znak != spacja
                 //jeśli nie, dodaje spacje
                 listOfLetterContent .add(i+1, ' ');
             }
-        }
+       // }
     }
 
     //Zamiana litery na wielką po '.'
-    public void changeToCapitalLetter(){
-        for (int i = 0; i < listOfLetterContent.size(); i++){
+    public void changeToCapitalLetter(int i){
+       // for (int i = 0; i < listOfLetterContent.size() - 2; i++){
             char currentChar = listOfLetterContent.get(i);
 
             if (currentChar == '.'){
                 char upperChar = Character.toUpperCase(listOfLetterContent.get(i+2));
                 listOfLetterContent.set(i+2, upperChar);
             }
-        }
+       // }
     }
 
     public String charTooString(){
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,12 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
 
-        String content = "To jest przykład zdania    ,              123 . test";
+        String content = "To jest  przykład      zdania            ,             123.  test.kropka      sa. To jest przykład poprawnego zdania. to  jest już inne  ,  przecinek sie tu nie zgadza i kropka tez .zaczynamy od wielkiej litery.";
 
         Corrector corrector = new Corrector();
         corrector.correcting(content);
         System.out.println(corrector.charTooString());
-
-        //,trim() - usunięcie spacji z końca i początku
-
     }
 }
